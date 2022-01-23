dc0dffea395338a954e8e7746bd0c4e2a672089f
Anderson Oliveira <andersonreinilson@hotmail.com>
2022-01-23 17:46:37 -0300

    funcionalidades adicionadas

---
+++ crud_projeto/post/static/js_project/actions.js
@@ -0,0 +1,19 @@
+const counter = document.querySelector('.counter');
+const text_area = document.querySelector('#text_area');
+const btn_limpar = document.querySelector('#limpar');
+
+text_area.addEventListener('input', function(event){
+    let qtd_letras = text_area.value.toString();
+
+    if (event.keyCode === 8) {
+        counter.innerHTML = parseInt(qtd_letras.length);
+    } else {
+         counter.innerHTML = parseInt(qtd_letras.length);
+    }
+});
+
+btn_limpar.addEventListener('click', () => {
+    contador = 0;
+    counter.innerHTML = contador.toString();
+    text_area.value = "";
+});
